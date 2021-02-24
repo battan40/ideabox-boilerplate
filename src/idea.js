f5752d4a0f5c368e0913e3170cf25d533cad6851
Ryan Miller <ryandmiller3521@gmail.com>
2021-02-23 20:55:49 -0700

    Add inital functionality

--- src/idea.js
+++ src/idea.js
@@ -1,18 +1,34 @@
 class Idea {
-  constructor() {
-
+  constructor(title, body) {
+    this.title = title;
+    this.body = body;
+    this.id = generateRdmId();
+    this.favorite = false;
   }
 
   saveToStorage() {
-
+    var stringifiedObj = JSOn.stringify(this);
+    localStorage.setItem(this.id, stringifiedObj);
   }
 
   deleteFromStorage() {
-
+    localStorage.removeItem(this.id);
   }
 
   updateIdea() {
+    if(this.favorite){
+      this.favorite = false;
+    }else{
+      this.favorite = true;
+    }
+  }
 
+  generateRdmId(array){
+    if(array.length - 1 >= 1){
+      var rdmId = 1;
+    }else{
+      var rdmId = array[array.length - 1].id + 1;
+    }
   }
 }
 
