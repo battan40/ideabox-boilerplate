7202cdd74ae97f865591545021926d4ec94f5719
mcraig2342 <matthewcookecraig10@gmail.com>
2021-02-24 15:04:54 -0700

    Add event listener

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
 var saveButton = document.querySelector('#save');
 var titleInput = document.querySelector('#titleInput');
 var bodyInput = document.querySelector('#bodyInput');
+
+
+
+saveButton.addEventListener('click', createIdeaCard);
