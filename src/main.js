b2e2890eb209482f57affdc273c23208cae45d78
mcraig2342 <matthewcookecraig10@gmail.com>
2021-02-24 15:21:19 -0700

    Add functionality to create a new idea object

--- src/main.js
+++ src/main.js
@@ -5,3 +5,15 @@ var bodyInput = document.querySelector('#bodyInput');
 
 
 saveButton.addEventListener('click', createIdeaCard);
+
+
+function createIdeaCard() {
+  var newIdea = new Idea();
+  newIdea = createNewIdea();
+  console.log(newIdea);
+}
+
+function createNewIdea() {
+  var newIdea = new Idea(titleInput.value, bodyInput.value);
+  return newIdea;
+}
